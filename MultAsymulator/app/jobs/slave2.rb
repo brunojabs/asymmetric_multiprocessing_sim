31e5022bf87010c7e2e678969b9753f13278e12c
Bruno <bruno.silva@jazz.etc.br>
2013-10-08 18:11:31 -0300

    Fix names

--- MultAsymulator/app/jobs/slave2.rb
+++ MultAsymulator/app/jobs/slave2.rb
@@ -4,12 +4,16 @@ class Slave2
 
   def self.perform(process_id)
     process = FakeProcess.find(process_id)
-    puts "Ola, sou o slave 1, vou processar seu process.name"
+    puts "==========================================="
+    puts ""
+    puts "Ola, sou o slave 2, vou processar seu #{process.name}"
     prng = Random.new
     time = prng.rand(15..30)
     puts "Vou levar #{time} segundos ok? Aguarde por favor"
     sleep time
     puts "Olha só, terminei de processar o #{process.name}"
+    puts ""
+    puts "==========================================="
     process.done = true
     process.save
   end
--- MultAsymulator/app/jobs/slave4.rb
+++ MultAsymulator/app/jobs/slave4.rb
@@ -4,12 +4,16 @@ class Slave4
 
   def self.perform(process_id)
     process = FakeProcess.find(process_id)
+    puts "==========================================="
+    puts ""
     puts "Ola, sou o slave 4, vou processar seu #{process.name}"
     prng = Random.new
     time = prng.rand(15..30)
     puts "Vou levar #{time} segundos ok? Aguarde por favor"
     sleep time
     puts "Olha só, terminei de processar o #{process.name}"
+    puts ""
+    puts "==========================================="
     process.done = true
     process.save
   end
